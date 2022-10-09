841c7787d39d9ef7558dbe8618f9afb7b8dfb998
Test1 1 <williamh2461@gmail.com>
2022-10-09 01:35:36 -0400

    added a put route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -17,21 +17,40 @@ router.get('/:id', (req, res) => {
   Category.findByPk(req.params.id, {
     include: [Product]
   }).then(categoryData => res.json(categoryData))
-  
-
-  // Category.findByPk({
-  //   include:[Product,  category_id]
-  // }).then(categoryData => res.json(categoryData))
-   
   // be sure to include its associated Products})
 });
 
 router.post('/', (req, res) => {
+  Category.create({
+    category_name: req.body.category_name
+  }).then((newCategory) => {
+    res.json(newCategory);
+  }).catch((err) => {
+    res.json(err);
+  })
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+      id:req.params.id,
+    },
+  }
+).then((updatedCategory) => {
+    res.json(updatedCategory);
+}).catch((err) => {
+  res.json(err);
+})
+
+
+
+
 });
 
 router.delete('/:id', (req, res) => {
